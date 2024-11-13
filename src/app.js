9bdbbf55dd5f2850ae603d543da60ad49629f742
Marcos Sevilla Miñana <marcos.sevilla@carset.es>
2024-11-13 11:55:26 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,10 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  let number = 10;
+function generateExcuse() {
+  let who = ['Alvaro', 'Hector', 'Adrian', 'Carmen'];
+  let action = ['comió', 'destrozó', 'aplastó', 'rompió'];
+  let what = ['mi tarea', 'mi teléfono', 'el coche'];
+  let when = ['antes de la clase', 'mientras codificaba en bootstrap', 'mientras hacía el generador de excusas', 'durante mi almuerzo', 'mientras rezaba'];
 
-  if (number == 10) console.log("Hello Rigo from the console!");
-  console.log("Hol Spain91");
-};
+  
+  const randomWho = who[Math.floor(Math.random() * who.length)];
+  const randomAction = action[Math.floor(Math.random() * action.length)];
+  const randomWhat = what[Math.floor(Math.random() * what.length)];
+  const randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  // Concatenar las partes para formar la excusa completa
+  const randomExcuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+
+  // Establecer la excusa generada en el elemento HTML con id="excuse"
+  document.getElementById('excuse').innerHTML = randomExcuse;
+}
+
+
+window.onload = generateExcuse;
